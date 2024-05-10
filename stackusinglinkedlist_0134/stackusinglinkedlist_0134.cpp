94740df6baf5801e39529cfb9e882a9666bb3d27
spongebar-netizen <fatanjrz1@gmail.com>
2024-05-10 10:36:30 +0700

    void pop

--- stackusinglinkedlist_0134/stackusinglinkedlist_0134.cpp
+++ stackusinglinkedlist_0134/stackusinglinkedlist_0134.cpp
@@ -31,3 +31,10 @@ public:
 		return value;
 	}
 
+	void pop() {
+		if (isEmpty()) {
+			cout << "Stack is empty." << endl;
+		}
+		cout << "Popped value: " << top->data << endl;
+		top = top->next;
+	}
