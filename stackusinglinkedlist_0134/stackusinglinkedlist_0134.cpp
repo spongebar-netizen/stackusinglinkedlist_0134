b299ba5bdfb074a4dc6db6467355799ae816ced2
spongebar-netizen <fatanjrz1@gmail.com>
2024-05-10 10:35:46 +0700

    return

--- stackusinglinkedlist_0134/stackusinglinkedlist_0134.cpp
+++ stackusinglinkedlist_0134/stackusinglinkedlist_0134.cpp
@@ -23,3 +23,11 @@ public:
 	}
 
 	int push(int value) {
+		Node* newNode = new Node();
+		newNode->data = value;
+		newNode->next = top;
+		top = newNode;
+		cout << "Push value: " << value << endl;
+		return value;
+	}
+
