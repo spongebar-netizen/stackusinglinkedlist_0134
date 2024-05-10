742dc76667d83dba8fdec33808da661cab667e54
spongebar-netizen <fatanjrz1@gmail.com>
2024-05-10 10:37:57 +0700

    Node* current = top; while

--- stackusinglinkedlist_0134/stackusinglinkedlist_0134.cpp
+++ stackusinglinkedlist_0134/stackusinglinkedlist_0134.cpp
@@ -44,3 +44,9 @@ public:
 		if (top == NULL) {
 			cout << "List is empty." << endl;
 		}
+		else {
+			Node* current = top;
+			while (current != NULL) {
+				cout << current->data << "   " << endl;
+				current = current->next;
+			}
